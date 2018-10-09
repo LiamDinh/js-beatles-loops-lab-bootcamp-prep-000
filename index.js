e05702d2d06b76f9d1af53c2647978a05af68992
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-09 03:47:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(arrMusicians, arrInstru) {
 }
 
 function johnLennonFacts(array){
-  for( i = 0; i < array.length; i++) {
+  for(var i = 0; i < array.length; i++) {
     array[i] = `{array[i]}!!!`
   }
   return array 
