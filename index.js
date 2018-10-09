200dd6ecd6a0d6f693c01292960fccb6d763b889
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-09 03:43:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay(arrMusicians, arrInstru) {
+  var array = []
+  for (var i = 0; i < arrMusicians.length; i++) {
+    array.push(`${arrMusicians[i]} plays ${arrInstru[i]}`)
+  }
+  return array 
+}
